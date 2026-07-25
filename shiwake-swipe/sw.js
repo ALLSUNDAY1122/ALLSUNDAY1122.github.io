ca4045bebb4f651e73cc280ce5af65115d944cf7
ALLSUNDAY1122 <kohei3615@gmail.com>
2026-07-25 17:27:05 +0900

    fix: disable stale cache during freeze investigation

--- shiwake-swipe/sw.js
+++ shiwake-swipe/sw.js
@@ -1,38 +1,8 @@
-const CACHE_NAME = 'shiwake-swipe-v4';
-const ASSETS = [
-  './',
-  './index.html',
-  './styles.css',
-  './delight.css',
-  './tempo-fix.css',
-  './app.js',
-  './delight.js',
-  './tempo-fix.js',
-  './manifest.webmanifest',
-  './data/questions.json',
-  './privacy.html',
-  './support.html',
-  './icons/icon.svg',
-  './icons/icon-180.svg'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
+const CACHE_NAME = 'shiwake-swipe-disabled-v5';
+self.addEventListener('install', event => { self.skipWaiting(); });
 self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))));
-  self.clients.claim();
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key)))).then(() => self.clients.claim()));
 });
-
 self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-  event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
-      const copy = response.clone();
-      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
-      return response;
-    }).catch(() => caches.match('./index.html')))
-  );
+  if (event.request.method === 'GET') event.respondWith(fetch(event.request));
 });
