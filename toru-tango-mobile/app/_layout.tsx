336ce52520a7a3c3a5c7a753ebee0ee09bfc4b07
ALLSUNDAY1122 <kohei3615@gmail.com>
2026-07-26 18:25:10 +0900

    fix(toru-tango): use readable status bar style

--- toru-tango-mobile/app/_layout.tsx
+++ toru-tango-mobile/app/_layout.tsx
@@ -8,7 +8,7 @@ import { AppStoreProvider } from '@/src/context/AppStore';
 export default function RootLayout() {
   return (
     <AppStoreProvider>
-      <StatusBar style="auto" />
+      <StatusBar style="dark" />
       <Stack screenOptions={{ headerShown: false }} />
     </AppStoreProvider>
   );
