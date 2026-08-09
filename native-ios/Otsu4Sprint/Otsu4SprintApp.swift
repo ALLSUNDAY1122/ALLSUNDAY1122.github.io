b41d2d62a8ee50eee8f269c8567cdc7d8acbc7bd
ALLSUNDAY1122 <kohei3615@gmail.com>
2026-08-09 11:54:52 +0900

    Restore Otsu4 app entry to implemented native root

--- native-ios/Otsu4Sprint/Otsu4SprintApp.swift
+++ native-ios/Otsu4Sprint/Otsu4SprintApp.swift
@@ -2,11 +2,9 @@ import SwiftUI
 
 @main
 struct Otsu4SprintApp: App {
-    @StateObject private var purchaseStore = Otsu4PurchaseStore()
-
     var body: some Scene {
         WindowGroup {
-            Otsu4LearningView(purchaseStore: purchaseStore)
+            Otsu4NativeRootView()
         }
     }
 }
