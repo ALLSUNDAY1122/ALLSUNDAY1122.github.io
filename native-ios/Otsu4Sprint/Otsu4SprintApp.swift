901191cffbc5ebc2397b7bafbb7621f7abb4032f
ALLSUNDAY1122 <kohei3615@gmail.com>
2026-08-09 11:54:40 +0900

    Connect Otsu4 app entry to native learning flow

--- native-ios/Otsu4Sprint/Otsu4SprintApp.swift
+++ native-ios/Otsu4Sprint/Otsu4SprintApp.swift
@@ -2,9 +2,11 @@ import SwiftUI
 
 @main
 struct Otsu4SprintApp: App {
+    @StateObject private var purchaseStore = Otsu4PurchaseStore()
+
     var body: some Scene {
         WindowGroup {
-            Otsu4NativeRootView()
+            Otsu4LearningView(purchaseStore: purchaseStore)
         }
     }
 }
