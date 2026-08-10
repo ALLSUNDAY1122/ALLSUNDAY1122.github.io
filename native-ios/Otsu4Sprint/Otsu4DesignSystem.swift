7b9bf667f4fe06c4312367cde90703515eb1f844
ALLSUNDAY1122 <kohei3615@gmail.com>
2026-08-10 09:21:28 +0900

    Fix Otsu4 Dynamic Type theme color initializer typo

--- native-ios/Otsu4Sprint/Otsu4DesignSystem.swift
+++ native-ios/Otsu4Sprint/Otsu4DesignSystem.swift
@@ -169,7 +169,7 @@ extension Color {
             .sRGB,
             red: Double((hex >> 16) & 0xFF) / 255,
             green: Double((hex >> 8) & 0xFF) / 255,
-            blue: Double((hex & 0xFF) / 255,
+            blue: Double(hex & 0xFF) / 255,
             opacity: 1
         )
     }
