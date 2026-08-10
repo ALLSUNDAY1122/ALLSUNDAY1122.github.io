d098be4e46daa06037941ca37d35346f4b47b84c
ALLSUNDAY1122 <kohei3615@gmail.com>
2026-08-10 14:47:33 +0900

    NW native: inject StoreKit 2 purchase store

--- network-specialist-sprint/ios/NetworkSpecialist/NetworkSpecialistApp.swift
+++ network-specialist-sprint/ios/NetworkSpecialist/NetworkSpecialistApp.swift
@@ -3,11 +3,16 @@ import SwiftUI
 @main
 struct NetworkSpecialistApp: App {
     @StateObject private var store = LearningStore()
+    @StateObject private var purchases = PremiumPurchaseStore()
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(store)
+                .environmentObject(purchases)
+                .task {
+                    await purchases.prepare()
+                }
         }
     }
 }
