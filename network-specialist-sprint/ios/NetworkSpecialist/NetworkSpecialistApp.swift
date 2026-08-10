6d83d3cb9a90f6660d591240e44379937db76f08
ALLSUNDAY1122 <kohei3615@gmail.com>
2026-08-10 09:15:08 +0900

    NW native: inject learning store

--- network-specialist-sprint/ios/NetworkSpecialist/NetworkSpecialistApp.swift
+++ network-specialist-sprint/ios/NetworkSpecialist/NetworkSpecialistApp.swift
@@ -2,9 +2,12 @@ import SwiftUI
 
 @main
 struct NetworkSpecialistApp: App {
+    @StateObject private var store = LearningStore()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(store)
         }
     }
 }
