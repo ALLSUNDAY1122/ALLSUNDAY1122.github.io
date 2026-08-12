099b62c1e89df87cc5d7d75a1bcb8137efb055a3
ALLSUNDAY1122 <kohei3615@gmail.com>
2026-08-13 08:01:24 +0900

    test(hokenshi): lock 2026 exam policy

--- hokenshi-sprint/NativePackage/Tests/HokenshiSprintFeatureTests/HokenshiSprintFeatureTests.swift
+++ hokenshi-sprint/NativePackage/Tests/HokenshiSprintFeatureTests/HokenshiSprintFeatureTests.swift
@@ -37,6 +37,37 @@ final class HokenshiSprintFeatureTests: XCTestCase {
         XCTAssertEqual(HokenshiSprintConfiguration.plannedMockExamCount, 3)
         XCTAssertEqual(HokenshiSprintConfiguration.questionsPerMockExam, 110)
         XCTAssertEqual(HokenshiSprintConfiguration.plannedIndependentQuestionCount, 330)
+        XCTAssertEqual(HokenshiSprintConfiguration.plannedQuestionsPerSubjectPerMock, 11)
+        XCTAssertEqual(
+            HokenshiSprintConfiguration.plannedQuestionsPerSubjectPerMock * HokenshiExamBlueprint.current.subjects.count,
+            HokenshiSprintConfiguration.questionsPerMockExam
+        )
+    }
+
+    func test2026ImprovementReportKeepsCurrentFramework() {
+        XCTAssertEqual(HokenshiExamPolicy2026.evidenceDate, "2026-03-19")
+        XCTAssertFalse(HokenshiExamPolicy2026.standardRevisionRequired)
+        XCTAssertTrue(HokenshiExamPolicy2026.keepCurrentQuestionCount)
+        XCTAssertTrue(HokenshiExamPolicy2026.keepCurrentExamTime)
+        XCTAssertTrue(HokenshiExamPolicy2026.latestLawAndStatisticsShouldBeTested)
+        XCTAssertTrue(HokenshiExamPolicy2026.visualMaterialShouldContinue)
+        XCTAssertTrue(HokenshiExamPolicy2026.communityDiagnosisShouldUseData)
+    }
+
+    func testSituationalQuestionPlanReproducesThirtyFiveQuestions() {
+        XCTAssertEqual(HokenshiExamPolicy2026.situationalScenarioGroupSizes.count, 12)
+        XCTAssertEqual(HokenshiExamPolicy2026.situationalScenarioGroupSizes.filter { $0 == 3 }.count, 11)
+        XCTAssertEqual(HokenshiExamPolicy2026.situationalScenarioGroupSizes.filter { $0 == 2 }.count, 1)
+        XCTAssertEqual(HokenshiExamPolicy2026.plannedSituationalQuestionCount, 35)
+        XCTAssertEqual(
+            HokenshiExamPolicy2026.plannedSituationalQuestionCount,
+            HokenshiExamBlueprint.current.situationalQuestions
+        )
+    }
+
+    func testTaxonomyPolicyMatchesImprovementReport() {
+        XCTAssertEqual(HokenshiExamPolicy2026.generalTaxonomies, ["I", "I-prime", "II"])
+        XCTAssertEqual(HokenshiExamPolicy2026.situationalTaxonomies, ["II", "III"])
     }
 
     func testSupportedOfficialAnswerPatternsMapToCore() {
