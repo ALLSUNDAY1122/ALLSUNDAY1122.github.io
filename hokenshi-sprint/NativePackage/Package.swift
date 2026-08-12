c410db473a9632967fc0ab4ea7f7c4b24b1f9c72
ALLSUNDAY1122 <kohei3615@gmail.com>
2026-08-12 13:57:51 +0900

    Add Hokenshi native Swift package

---
+++ hokenshi-sprint/NativePackage/Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "HokenshiSprintNative",
+    platforms: [
+        .iOS(.v16),
+        .macOS(.v13)
+    ],
+    products: [
+        .library(name: "HokenshiSprintFeature", targets: ["HokenshiSprintFeature"])
+    ],
+    dependencies: [
+        .package(path: "../../native-ios/LearningSprintCore")
+    ],
+    targets: [
+        .target(
+            name: "HokenshiSprintFeature",
+            dependencies: ["LearningSprintCore"]
+        ),
+        .testTarget(
+            name: "HokenshiSprintFeatureTests",
+            dependencies: ["HokenshiSprintFeature", "LearningSprintCore"]
+        )
+    ]
+)
