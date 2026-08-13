3833f45ee1975283d68b9e6be01d5e470979bc49
ALLSUNDAY1122 <kohei3615@gmail.com>
2026-08-13 20:52:05 +0900

    feat(hokenshi): add native iOS app entry point

---
+++ hokenshi-sprint/ios/HokenshiSprintApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+import HokenshiSprintFeature
+
+@main
+struct HokenshiSprintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HokenshiRootView()
+        }
+    }
+}
