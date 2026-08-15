5ae93fd27b8e558385e44c105d1f03379baab162
ALLSUNDAY1122 <kohei3615@gmail.com>
2026-08-15 13:15:06 +0900

    feat(mesh): inject mesh scan model into app shell

--- splat-native-ios/SplatNative/SplatNativeApp.swift
+++ splat-native-ios/SplatNative/SplatNativeApp.swift
@@ -3,11 +3,13 @@ import SwiftUI
 @main
 struct SplatNativeApp: App {
     @StateObject private var model = ScanModel()
+    @StateObject private var meshModel = MeshScanModel()
 
     var body: some Scene {
         WindowGroup {
             RootScanView()
                 .environmentObject(model)
+                .environmentObject(meshModel)
         }
     }
 }
