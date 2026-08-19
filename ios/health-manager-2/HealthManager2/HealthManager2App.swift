9e7e085bb1b0d057fc8ebf5971206b26ecfd4b40
ALLSUNDAY1122 <kohei3615@gmail.com>
2026-08-19 15:53:12 +0900

    feat(hm2): inject StoreKit manager

--- ios/health-manager-2/HealthManager2/HealthManager2App.swift
+++ ios/health-manager-2/HealthManager2/HealthManager2App.swift
@@ -2,9 +2,11 @@ import SwiftUI
 
 @main
 struct HealthManager2App: App {
+    @StateObject private var store = StoreKitManager()
+
     var body: some Scene {
         WindowGroup {
-            LocalWebView()
+            LocalWebView(store: store)
                 .ignoresSafeArea()
         }
     }
