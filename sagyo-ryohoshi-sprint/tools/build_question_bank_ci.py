39d44fa39a937a71078a427fd9b9f4e7593cc2cd
ALLSUNDAY1122 <kohei3615@gmail.com>
2026-08-21 18:47:56 +0900

    fix: pin verified R59 OT official PDFs in CI

--- sagyo-ryohoshi-sprint/tools/build_question_bank_ci.py
+++ sagyo-ryohoshi-sprint/tools/build_question_bank_ci.py
@@ -23,6 +23,18 @@ R60_OT_ANSWER = (
     "https://www.mhlw.go.jp/seisakunitsuite/bunya/kenkou_iryou/iryou/topics/dl/"
     "tp250428-09seitou.pdf"
 )
+R59_PAGE = (
+    "https://www.mhlw.go.jp/seisakunitsuite/bunya/kenkou_iryou/iryou/topics/"
+    "tp240424-08_09.html"
+)
+R59_OT_AM = (
+    "https://www.mhlw.go.jp/seisakunitsuite/bunya/kenkou_iryou/iryou/topics/dl/"
+    "tp240424-09a_01.pdf"
+)
+R59_OT_PM = (
+    "https://www.mhlw.go.jp/seisakunitsuite/bunya/kenkou_iryou/iryou/topics/dl/"
+    "tp240424-09b_01.pdf"
+)
 CORRECTED_R59_OT_ANSWER = (
     "https://www.mhlw.go.jp/general/sikaku/successlist/2024/"
     "siken08_09-2/dl/OT_seitou.pdf"
@@ -34,6 +46,8 @@ _original_pdf_links_from_page = bank.pdf_links_from_page
 def pdf_links_from_page(page_url):
     if page_url == R60_PAGE:
         return R60_OT_AM, R60_OT_PM, R60_OT_ANSWER
+    if page_url == R59_PAGE:
+        return R59_OT_AM, R59_OT_PM, CORRECTED_R59_OT_ANSWER
     return _original_pdf_links_from_page(page_url)
 
 
