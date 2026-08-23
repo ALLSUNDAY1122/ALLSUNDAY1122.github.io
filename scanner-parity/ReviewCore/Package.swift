5b6dd5a5dfc10030803395b8f38a1c2c6dfafb08
ALLSUNDAY1122 <kohei3615@gmail.com>
2026-08-23 17:31:33 +0900

    scanner parity: package review core for app integration

---
+++ scanner-parity/ReviewCore/Package.swift
@@ -0,0 +1,11 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+let package = Package(
+    name: "ScannerParityReviewCore",
+    platforms: [.iOS(.v17), .macOS(.v14)],
+    products: [.library(name: "ReviewCore", targets: ["ReviewCore"])],
+    targets: [
+        .target(name: "ReviewCore", path: ".", exclude: ["Package.swift", "README.md"], sources: ["ReviewQueueCore.swift"])
+    ]
+)
