a6caf46951dbacd7e6bb4861a415f92b83108600
ALLSUNDAY1122 <kohei3615@gmail.com>
2026-09-11 23:16:12 +0900

    Keep mesh asset contract independently testable

--- splat-native-ios/SplatNative/MeshAssetContract.swift
+++ splat-native-ios/SplatNative/MeshAssetContract.swift
@@ -151,20 +151,3 @@ enum MeshAssetContract {
         return .unknown
     }
 }
-
-extension MeshScanModel {
-    var exporterMeshAsset: MeshAssetDescriptor? {
-        guard let resultURL else { return nil }
-        return MeshAssetContract.descriptor(for: resultURL)
-    }
-
-    var currentMeshHasMetricScale: Bool {
-        exporterMeshAsset?.hasMetricScale ?? false
-    }
-
-    @discardableResult
-    func persistExporterMeshAssetContract() throws -> URL? {
-        guard let descriptor = exporterMeshAsset else { return nil }
-        return try MeshAssetContract.writeSidecar(for: descriptor)
-    }
-}
