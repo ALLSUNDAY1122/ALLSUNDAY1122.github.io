375d8392ae7f67b47bef6420fdfcdb80ea403d74
ALLSUNDAY1122 <kohei3615@gmail.com>
2026-09-12 06:16:01 +0900

    Align RAW validator regressions with minimum image dimensions

--- splat-native-ios/SplatNativeTests/MeshRawInputValidatorTests.swift
+++ splat-native-ios/SplatNativeTests/MeshRawInputValidatorTests.swift
@@ -2,6 +2,7 @@ import Foundation
 import XCTest
 
 final class MeshRawInputValidatorTests: XCTestCase {
+    private let usablePNG = Data(base64Encoded: "iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAIAAAAlC+aJAAAATUlEQVR42u3PQQ0AAAgEIDX5RTeFDzdoQCepz6aeExAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQELi3oiwCAJt186UAAAAASUVORK5CYII=")!
     private let onePixelPNG = Data(base64Encoded: "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII=")!
 
     func testRejectsRenamedCorruptImageBytes() throws {
@@ -14,10 +15,20 @@ final class MeshRawInputValidatorTests: XCTestCase {
         )
     }
 
-    func testAcceptsStructurallyReadableImage() throws {
+    func testRejectsStructurallyReadableButImplausiblyTinyImage() throws {
         let directory = try makeDirectory()
         defer { try? FileManager.default.removeItem(at: directory) }
-        try onePixelPNG.write(to: directory.appendingPathComponent("valid.png"))
+        try onePixelPNG.write(to: directory.appendingPathComponent("tiny.png"))
+
+        XCTAssertFalse(
+            MeshRawInputValidator.hasMinimumUsableImages(in: directory, minimumCount: 1)
+        )
+    }
+
+    func testAcceptsStructurallyReadableImageAboveMinimumDimensions() throws {
+        let directory = try makeDirectory()
+        defer { try? FileManager.default.removeItem(at: directory) }
+        try usablePNG.write(to: directory.appendingPathComponent("valid.png"))
 
         XCTAssertTrue(
             MeshRawInputValidator.hasMinimumUsableImages(in: directory, minimumCount: 1)
@@ -29,11 +40,11 @@ final class MeshRawInputValidatorTests: XCTestCase {
         defer { try? FileManager.default.removeItem(at: directory) }
 
         for index in 0..<19 {
-            try onePixelPNG.write(to: directory.appendingPathComponent("frame_\(index).png"))
+            try usablePNG.write(to: directory.appendingPathComponent("frame_\(index).png"))
         }
         XCTAssertFalse(MeshRawInputValidator.hasMinimumUsableImages(in: directory))
 
-        try onePixelPNG.write(to: directory.appendingPathComponent("frame_19.png"))
+        try usablePNG.write(to: directory.appendingPathComponent("frame_19.png"))
         XCTAssertTrue(MeshRawInputValidator.hasMinimumUsableImages(in: directory))
         XCTAssertEqual(MeshRawInputValidator.minimumPhotogrammetryImageCount, 20)
     }
